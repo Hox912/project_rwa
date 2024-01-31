06ea5b5dd76060eca5141cd72fd9dc3849b50a2c
Hox912 <hokeivan@gmail.com>
2024-01-31 17:51:44 +0100

    create and add OAuth com

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,7 +15,13 @@ const userSchema = new mongoose.Schema ({
         type: String,
         required: true,
     },
-}, { timestamps: true });
+    avatar: {
+        type: String,
+        default: "https://static.vecteezy.com/system/resources/previews/005/129/844/non_2x/profile-user-icon-isolated-on-white-background-eps10-free-vector.jpg"
+    },
+    }, 
+    { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
 
