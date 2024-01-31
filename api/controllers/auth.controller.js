3ba47abd38cb26c65b55264c7e10515e033cc3f4
Hox912 <hokeivan@gmail.com>
2024-01-31 15:46:39 +0100

    complete sign in page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -10,7 +10,7 @@ export const signup = async (req, res, next) => {
     
     try {
         await newUser.save()
-        res.status(201).json('User created sucessfully'); 
+        res.status(201).json('Uspjesno kreiran racun!'); 
     } catch (error) {
         next(error);
     }
@@ -20,9 +20,9 @@ export const signin = async (req, res, next) => {
     const { email, password } = req.body;
     try {
         const validUser = await User.findOne({ email });
-        if(!validUser) return next(errorHandler(404, 'User not found!'));
+        if(!validUser) return next(errorHandler(404, 'Korisnik nije pronaden!'));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if(!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+        if(!validPassword) return next(errorHandler(401, 'Krivo uneseni podaci!'));
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
         const { password: pass, ...rest } = validUser._doc;
         res
