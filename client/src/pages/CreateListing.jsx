6cd140908fc859c6b50d42c6c78322ecb7a06df9
Hox912 <hokeivan@gmail.com>
2024-02-01 13:20:20 +0100

    listing page functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -2,14 +2,25 @@ import { useState } from 'react'
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
 import { set } from 'mongoose';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 export default function CreateListing() {
+    const {currentUser} = useSelector(state => state.user)
     const [files, setFiles] = useState([]);
     const [formData, setFormData] = useState({
     imageUrls: [],
+    name: '',
+    description: '',
+    address: '',
+    type: 'rent',
+    regularPrice: 50,
+    discountPrice: 50,
     });
     const [imageUploadError, setImageUploadError] = useState(false);
     const [uploading, setUploading] = useState(false);
+    const[error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
     console.log(formData);
     const handleImageSubmit = (e) => {
         if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
@@ -71,33 +82,76 @@ export default function CreateListing() {
     });
   };
 
+  const handleChange = (e) => {
+    if(e.target.id === 'sale' || e.target.id === 'rent') {
+        setFormData({
+            ...formData,
+            type: e.target.id
+        })
+    }
+
+    if(e.target.type === 'number' || e.target.type === 'text' || e.target.type === 'textarea'){
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value,
+        });
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+        if(formData.imageUrls.length < 1) return setError('Morate ucitati makar jednu sliku');
+        setLoading(true);
+        setError(false);
+        const res = await fetch('/api/listing/create', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                ...formData,
+                userRef: currentUser._id
+            }),
+        });
+        const data = await res.json();
+        setLoading(false);
+        if(data.success === false) {
+            setError(data.message);
+        }
+    } catch (error) {
+        setError(error.message);
+        setLoading(false);
+    }
+  }
+
   return (
     <main className='p-3 max-w-4xl mx-auto'>
         <h1 className='text-3xl font-semibold text-center my-7'>Objavite oglas</h1>
-        <form className='flex flex-col sm:flex-row gap-4'>
+        <form onSubmit={handleSubmit} className='flex flex-col sm:flex-row gap-4'>
             <div className='flex flex-col gap-4 flex-1'>
-                <input type="text" placeholder='Ime' className='border p-3 rounded-lg' id='name' maxLength='62' minLength='5' required />
-                <textarea type="text" placeholder='Opis' className='border p-3 rounded-lg' id='description' required />
-                <input type="text" placeholder='Adresa (mjesto pokupa)' className='border p-3 rounded-lg' id='address' required />
+                <input type="text" placeholder='Ime' className='border p-3 rounded-lg' id='name' maxLength='62' minLength='5' required onChange={handleChange} value={formData.name}/>
+                <textarea type="text" placeholder='Opis' className='border p-3 rounded-lg' id='description' required onChange={handleChange} value={formData.description}/>
+                <input type="text" placeholder='Adresa (mjesto pokupa)' className='border p-3 rounded-lg' id='address' required onChange={handleChange} value={formData.address} />
 
                 <div className='flex gap-6 flex-wrap'>
                    <div className='flex gap-2'>
-                        <input type='checkbox' id='sale' className='w-5' />
+                        <input type='checkbox' id='sale' className='w-5' onChange={handleChange} checked={formData.type === 'sale'}/>
                         <span>Na prodaju</span>
                    </div>
                    <div className='flex gap-2'>
-                        <input type='checkbox' id='sale' className='w-5' />
+                        <input type='checkbox' id='sale' className='w-5' onChange={handleChange} checked={formData.type === 'rent'}/>
                         <span>Za iznajmljivanje</span>
                    </div>
                    <div className='flex items-center gap-2'>
-                        <input type="number" id='regularPrice' min='1' max='10' required className='p-3 border border-gray-300 rounded-lg' /> 
+                        <input type="number" id='regularPrice' min='50' max='1000000' required className='p-3 border border-gray-300 rounded-lg' onChange={handleChange} value={formData.regularPrice}/> 
                         <div className='flex flex-col items-center'>
                             <p>Normalna cijena</p>  
                             <span className='text-xs'>(€ / dnevno)</span> 
                         </div> 
                    </div> 
                    <div className='flex items-center gap-2'>
-                        <input type="number" id='discountPrice' min='1' max='10' required className='p-3 border border-gray-300 rounded-lg' /> 
+                        <input type="number" id='discountPrice' min='50' max='1000000' required className='p-3 border border-gray-300 rounded-lg' onChange={handleChange} checked={formData.discountPrice}/> 
                         <div className='flex flex-col items-center'>
                             <p>Cijena s popustom</p>  
                             <span className='text-xs'>(€ / dnevno)</span> 
@@ -132,13 +186,14 @@ export default function CreateListing() {
                   onClick={() => handleRemoveImage(index)}
                   className='p-3 text-red-700 rounded-lg uppercase hover:opacity-75'
                 >
-                  Delete
+                  Obrisite
                 </button>
               </div>
             ))}
           <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-            Create Listing
+            {loading ? 'Kreiranje...' : 'Objavi oglas'}
           </button>
+          {error && <p className='text-red-700 text-sm'>{error}</p>}
             </div>
         </form>
     </main>
